b7ae149e8f90d40a78dc7e2f1913edb8928fd301
Bailey Vogt <s3906263@student.rmit.edu.au>
2025-07-03 23:08:22 +1000

    add bluetooth support

---
+++ modules/nixos/bluetooth.nix
@@ -0,0 +1,24 @@
+{
+
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
+  };
+
+
+
+  # Enable blueman service
+  services.blueman.enable = true;
+
+
+
+
+
+}
