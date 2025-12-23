08a898f8e03ae46ea55132c008c67f28f93de622
Bailey Vogt <s3906263@student.rmit.edu.au>
2025-12-23 15:04:01 +1100

    add vim config deps

--- modules/home-manager/vim.nix
+++ modules/home-manager/vim.nix
@@ -1,5 +1,12 @@
 { pkgs, inputs, config, ... }: {
 
+
+  home.packages = with pkgs; [
+    clang-tools
+    jdt-language-server
+    lua-language-server
+  ];
+
   imports = [
     inputs.nvimConfig.outputs.homeManagerModules.nvimConfig
   ];
